3f42c25c6842d536f0e1917f2c33516099ab01ab
Robert Greener <robert.greener@kcl.ac.uk>
2018-02-08 17:48:52 +0000

    Allow address_line_2 in Practitioners to be null

--- connect_therapy/models.py
+++ connect_therapy/models.py
@@ -17,7 +17,7 @@ class Patient(models.Model):
 class Practitioner(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address_line_1 = models.CharField(max_length=100)
-    address_line_2 = models.CharField(max_length=100)
+    address_line_2 = models.CharField(max_length=100, blank=True, null=True)
     postcode = models.CharField(max_length=10)
     mobile = models.CharField(max_length=20)
     bio = models.TextField()
