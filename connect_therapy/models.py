afb96ee362b14febb336cc17f92082d83b378b03
Robert Greener <robert.greener@kcl.ac.uk>
2018-02-08 17:07:09 +0000

    Add Appointment model to models.py

--- connect_therapy/models.py
+++ connect_therapy/models.py
@@ -15,3 +15,27 @@ class Practitioner(models.Model):
     address_line_2 = models.CharField(max_length=100)
     postcode = models.CharField(max_length=20)
     bio = models.TextField()
+
+
+class Appointment(models.Model):
+    practitioner = models.ForeignKey(Practitioner,
+                                     on_delete=models.SET_NULL,
+                                     null=True)
+    patient = models.ForeignKey(Patient,
+                                on_delete=models.CASCADE,
+                                null=True,
+                                blank=True)
+    start_date_and_time = models.DateTimeField()
+    length = models.TimeField()
+    """This is how long the appointment lasts"""
+    practitioner_notes = models.TextField(blank=True)
+    """These are notes left by the practitioner at the end of the appointment,
+    that should only be visible to the practitioner"""
+    patient_notes_by_practitioner = models.TextField(blank=True)
+    """These are notes left by the practitioner at the end of the appointment,
+    visible to the patient
+    """
+    patient_notes_before_meeting = models.TextField(blank=True)
+    """These are notes left before the appointment by the patient,
+    for the benefit of the practitioner
+    """
