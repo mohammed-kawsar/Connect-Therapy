9da4a3b25dbdfb3473f7382582b3a8ec8dc138dd
Robert Greener <robert.greener@kcl.ac.uk>
2018-02-08 16:51:30 +0000

    Add Practitioner model to models.py

--- connect_therapy/models.py
+++ connect_therapy/models.py
@@ -7,3 +7,11 @@ class Patient(models.Model):
     gender = models.CharField(max_length=1)
     mobile = models.CharField(max_length=20)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Practitioner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address_line_1 = models.CharField(max_length=100)
+    address_line_2 = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=20)
+    bio = models.TextField()
