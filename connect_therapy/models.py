5d680c251b58a972abc6d7fb730724027a3dd99b
Robert Greener <robert.greener@kcl.ac.uk>
2018-02-08 17:19:46 +0000

    Fix wrongly implemented Practitioner
    Change postcode length
    Add mobile

--- connect_therapy/models.py
+++ connect_therapy/models.py
@@ -13,7 +13,8 @@ class Practitioner(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address_line_1 = models.CharField(max_length=100)
     address_line_2 = models.CharField(max_length=100)
-    postcode = models.CharField(max_length=20)
+    postcode = models.CharField(max_length=10)
+    mobile = models.CharField(max_length=20)
     bio = models.TextField()
 
 
