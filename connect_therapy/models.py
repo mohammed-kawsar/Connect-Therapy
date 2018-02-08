ee48984c8575a74bc062dbac1246b133cfc46d40
Robert Greener <robert.greener@kcl.ac.uk>
2018-02-08 16:49:09 +0000

    Add Patient model to models.py

--- connect_therapy/models.py
+++ connect_therapy/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Patient(models.Model):
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=1)
+    mobile = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
