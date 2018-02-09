8d9074aee524d4496bf6df8d6ed99bd38b5266a8
Robert Greener <robert.greener@kcl.ac.uk>
2018-02-09 14:44:54 +0000

    Add PatientLoginFormTests

--- connect_therapy/tests.py
+++ connect_therapy/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from connect_therapy.models import Patient, Practitioner, Appointment
-from connect_therapy.forms import PatientSignUpForm
+from connect_therapy.forms import PatientSignUpForm, PatientLoginForm
 from datetime import date, datetime, time
 import pytz
 
@@ -124,3 +124,64 @@ class PatientSignUpFormTests(TestCase):
             'password2': 'meowmeow1'
         })
         self.assertTrue(form.is_valid())
+
+
+class PatientLoginFormTests(TestCase):
+    def test_when_patient_exists_and_valid(self):
+        user = User.objects.create_user(username='test@example.com',
+                                        password='woofwoof12'
+                                        )
+        patient = Patient(user=user,
+                          date_of_birth=date(year=1995, month=2, day=20),
+                          gender='M',
+                          mobile='+447476565333'
+                          )
+        patient.save()
+        form = PatientLoginForm(data= {
+            'username': 'test@example.com',
+            'password': 'woofwoof12'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_when_patient_does_not_exist(self):
+        user = User.objects.create_user(username='test@example.com',
+                                        password='woofwoof12'
+                                        )
+        form = PatientLoginForm(data={
+            'username': 'test@example.com',
+            'password': 'woofwoof12'
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_when_password_invalid(self):
+        user = User.objects.create_user(username='test@example.com',
+                                        password='woofwoof12'
+                                        )
+        patient = Patient(user=user,
+                          date_of_birth=date(year=1995, month=2, day=20),
+                          gender='M',
+                          mobile='+447476565333'
+                          )
+        patient.save()
+        form = PatientLoginForm(data={
+            'username': 'test@example.com',
+            'password': 'woofwoof11'
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_when_username_invalid(self):
+        user = User.objects.create_user(username='test@example.com',
+                                        password='woofwoof12'
+                                        )
+        patient = Patient(user=user,
+                          date_of_birth=date(year=1995, month=2, day=20),
+                          gender='M',
+                          mobile='+447476565333'
+                          )
+        patient.save()
+        form = PatientLoginForm(data={
+            'username': 'test@demo.com',
+            'password': 'woofwoof12'
+        })
+        self.assertFalse(form.is_valid())
+
