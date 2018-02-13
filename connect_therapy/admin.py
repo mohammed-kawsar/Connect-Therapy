fcddb50fc03947235c684572833a51bf139c48c6
Robert Greener <robert.greener@kcl.ac.uk>
2018-02-13 20:24:32 +0000

    Add Practitioner to Admin View

--- connect_therapy/admin.py
+++ connect_therapy/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
+from connect_therapy.models import Practitioner
+
+
+@admin.register(Practitioner)
+class PractitionerAdmin(admin.ModelAdmin):
+    first_name = lambda x: x.user.first_name
+    first_name.short_description = "First Name"
+    last_name = lambda x: x.user.last_name
+    last_name.short_description = "Last Name"
+    email = lambda x: x.user.email
+    email.short_description = "Email"
+    list_display = (first_name, last_name, email, 'is_approved')
+
+
 admin.site.unregister(Group)
