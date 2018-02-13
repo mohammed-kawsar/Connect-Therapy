0db6fd326b27207e3e929e61cc08371d56a61999
Robert Greener <robert.greener@kcl.ac.uk>
2018-02-13 20:25:33 +0000

    Add mark approved and not approved to Practitioner admin interface

--- connect_therapy/admin.py
+++ connect_therapy/admin.py
@@ -4,6 +4,20 @@ from django.contrib.auth.models import Group
 from connect_therapy.models import Practitioner
 
 
+def mark_approved(modeladmin, request, queryset):
+    queryset.update(is_approved=True)
+
+
+mark_approved.short_description = "Mark as approved"
+
+
+def mark_not_approved(modeladmin, request, queryset):
+    queryset.update(is_approved=False)
+
+
+mark_not_approved.short_description = "Mark as not approved"
+
+
 @admin.register(Practitioner)
 class PractitionerAdmin(admin.ModelAdmin):
     first_name = lambda x: x.user.first_name
@@ -14,6 +28,7 @@ class PractitionerAdmin(admin.ModelAdmin):
     email.short_description = "Email"
     list_display = (first_name, last_name, email, 'is_approved')
     list_filter = ('is_approved',)
+    actions = (mark_approved, mark_not_approved)
 
 
 admin.site.unregister(Group)
