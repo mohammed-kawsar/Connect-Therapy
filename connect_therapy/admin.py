e5b493738c3c1da20e23f8ea5aa51b258b2e4167
K1631313 <muhammed.hasan@kcl.ac.uk>
2018-03-02 22:47:38 +0000

    Remove blank line

--- connect_therapy/admin.py
+++ connect_therapy/admin.py
@@ -38,7 +38,6 @@ class PractitionerAdmin(admin.ModelAdmin):
 
 admin.site.unregister(Group)
 admin.site.unregister(User)
-
 admin.site.site_header = 'Connect Therapy Administration'
 admin.site.site_title = 'Connect Therapy - Admin'
 
