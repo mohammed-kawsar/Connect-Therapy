6185a4a9ac9173be2c2f261394e6b2dec4f617fc
Robert Greener <robert.greener@kcl.ac.uk>
2018-03-04 12:13:25 +0000

    Add notification hooks

---
+++ connect_therapy/notifications.py
@@ -0,0 +1,40 @@
+from threading import Thread
+from connect_therapy.emails import *
+
+
+def appointment_booked(appointment):
+    threads = [
+        Thread(target=send_patient_appointment_booked, args=(appointment, )),
+        Thread(target=send_practitioner_appointment_booked,
+               args=(appointment, )
+               ),
+    ]
+
+    for thread in threads:
+        thread.start()
+
+
+def reminders():
+    threads = [
+        Thread(target=send_patient_appointment_reminders),
+        Thread(target=send_practitioner_appointment_reminders),
+    ]
+
+    for thread in threads:
+        thread.start()
+
+
+def appointment_cancelled_by_patient(patient, appointment, under_24h=False):
+    """This must be called BEFORE the appointment is actually cancelled"""
+    if under_24h:
+        send_patient_cancelled_under_24_hours(patient, appointment)
+        send_practitioner_patient_cancelled_in_under_24_hours(appointment)
+    else:
+        send_patient_cancelled_in_good_time(patient, appointment)
+        send_practitioner_patient_cancelled_in_good_time(appointment)
+
+
+def appointment_cancelled_by_practitioner(appointment):
+    """This must be called BEFORE the appointment is actually cancelled"""
+    send_patient_practitioner_has_cancelled(appointment)
+    send_practitioner_cancelled(appointment)
