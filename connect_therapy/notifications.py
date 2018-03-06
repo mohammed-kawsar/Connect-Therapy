411b1e1b6bd095d901c3ab335250b9a2e0ff0388
Robert Greener <robert.greener@kcl.ac.uk>
2018-03-06 19:21:32 +0000

    Link sms to notifications.py

--- connect_therapy/notifications.py
+++ connect_therapy/notifications.py
@@ -1,10 +1,12 @@
-from threading import Thread
 from connect_therapy.emails import *
+from connect_therapy import sms
 
 
 def appointment_booked(appointment):
     send_practitioner_appointment_booked(appointment)
     send_patient_appointment_booked(appointment)
+    sms.send_appointment_booked(appointment.patient, appointment)
+    sms.send_appointment_booked(appointment.practitioner, appointment)
 
 
 def multiple_appointments_booked(appointments):
@@ -15,6 +17,8 @@ def multiple_appointments_booked(appointments):
 def reminders():
     send_patient_appointment_reminders()
     send_practitioner_appointment_reminders()
+    sms.send_patient_appointment_reminders()
+    sms.send_practitioner_appointment_reminders()
 
 
 def appointment_cancelled_by_patient(patient, appointment, under_24h=False):
@@ -25,9 +29,13 @@ def appointment_cancelled_by_patient(patient, appointment, under_24h=False):
     else:
         send_patient_cancelled_in_good_time(patient, appointment)
         send_practitioner_patient_cancelled_in_good_time(appointment)
+    sms.send_appointment_cancelled(appointment.patient, appointment)
+    sms.send_appointment_cancelled(appointment.practitioner, appointment)
 
 
 def appointment_cancelled_by_practitioner(appointment):
     """This must be called BEFORE the appointment is actually cancelled"""
     send_patient_practitioner_has_cancelled(appointment)
     send_practitioner_cancelled(appointment)
+    sms.send_appointment_cancelled(appointment.patient, appointment)
+    sms.send_appointment_cancelled(appointment.practitioner, appointment)
