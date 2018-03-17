5e8e8e5bbfa4a887385c325896f3d25705a566e6
K1631313 <muhammed.hasan@kcl.ac.uk>
2018-03-17 23:01:52 +0000

    Add back import removed by mistake

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "sslserver",  # used to test video chat
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+)
 
 ROOT_URLCONF = 'mysite.urls'
 
