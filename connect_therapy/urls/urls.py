f80befc6180a5ddb2508e6589b94fed89dad81cb
Robert Greener <robert.greener@kcl.ac.uk>
2018-03-18 18:07:42 +0000

    Fix activate URL

--- connect_therapy/urls/urls.py
+++ connect_therapy/urls/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
          ),
          name='index'
          ),
-    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-            activate,
-            name='activate')
+    path('activate/<uidb64>/<token>',
+         activate,
+         name='activate')
 ]
