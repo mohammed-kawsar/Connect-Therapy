5c2755250a47b2fab636bb78df7dc74a17f00b52
Jaman Salique <jaman.salique@kcl.ac.uk>
2018-03-24 16:49:38 +0000

    Override password reset admin templates.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,25 +1,27 @@
 from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import password_reset, password_reset_complete, password_reset_done, \
+    password_reset_confirm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('connect_therapy.urls.urls')),
     path('password_reset/',
-         auth_views.PasswordResetView.as_view(),
+         password_reset,
          {'template_name': 'connect_therapy/password_reset_form.html'},
          name='password_reset'
          ),
     path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(),
+         password_reset_done, {'template_name': 'connect_therapy/password_reset_done.html'},
          name='password_reset_done'
          ),
     path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(),
+         password_reset_confirm, {'template_name': 'connect_therapy/password_reset_confirm.html'},
          name='password_reset_confirm'
          ),
     path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(),
+         password_reset_complete, {'template_name': 'connect_therapy/password_reset_complete.html'},
          name='password_reset_complete'
          ),
 ]
