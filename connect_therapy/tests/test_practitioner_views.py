5376a0a9f012cc7e704d3e57af6bc076be0075c3
“Sakeen <sakeen.zaman@kcl.ac.uk>
2018-03-25 18:12:25 +0100

    Comment out more code that does not work

--- connect_therapy/tests/test_practitioner_views.py
+++ connect_therapy/tests/test_practitioner_views.py
@@ -6,8 +6,12 @@ from django.test import TestCase, Client, RequestFactory
 from django.contrib.auth.models import AnonymousUser
 
 from connect_therapy.forms.practitioner.practitioner import PractitionerNotesForm
-from connect_therapy.models import Practitioner, Appointment, Patient
 from connect_therapy.views.practitioner import *
+
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
+from connect_therapy.models import Patient, Appointment, Practitioner
 
 
 class PractitionerSignUpTest(TestCase):
@@ -153,27 +157,6 @@ class TestPractitionerNotes(TestCase):
         view = PractitionerNotesView()
         view.request = request
         self.assertFalse(view.test_func())
-
-    # def test_test_func_when_appointment_has_no_patient(self):
-    #     appointment = Appointment(patient=None,
-    #                               start_date_and_time=datetime.datetime(year=2018,
-    #                                                            month=4,
-    #                                                            day=17,
-    #                                                            hour=15,
-    #                                                            minute=10,
-    #                                                            tzinfo=pytz.utc),
-    #                               length=timedelta(hours=1))
-    #     appointment.save()
-    #
-    #     factory = RequestFactory()
-    #     request = factory.post(reverse_lazy('connect_therapy:practitioner-make-notes',
-    #                                         kwargs={'pk': appointment.pk}))
-    #     request.user = self.request.user.practitioner
-    #     view = PractitionerNotesView()
-    #     view.request = request
-    #     view.get_object = lambda queryset=None: appointment
-    #     self.assertFalse(view.test_func())
-
 
 class TestPractitionerAllPatientsView(TestCase):
     def test_unique_patient(self):
@@ -452,3 +435,130 @@ class PractitionerNotesTest(TestCase):
                          kwargs={'pk': 1}))
 
         self.assertEqual(response.status_code, 302)
+
+
+class PractitionerMyAppointmentsViewTest(TestCase):
+    def setUp(self):
+        test_user_1 = User.objects.create_user(username='testuser1')
+        test_user_1.set_password('12345')
+        test_user_1.save()
+        self.patient = Patient(user=test_user_1,
+                               gender='M',
+                               mobile="+447476666555",
+                               date_of_birth=date(year=1995, month=1, day=1))
+        self.patient.save()
+        self.appointment = Appointment(patient=self.patient,
+                                       start_date_and_time=datetime.datetime(year=2018,
+                                                                    month=4,
+                                                                    day=17,
+                                                                    hour=15,
+                                                                    minute=10,
+                                                                    tzinfo=pytz.utc),
+                                       length=timedelta(hours=1))
+        self.appointment.save()
+
+    # def test_test_func_when_appointment_has_no_patient(self):
+    #     appointment = Appointment(patient=None,
+    #                               start_date_and_time=datetime.datetime(year=2018,
+    #                                                            month=4,
+    #                                                            day=17,
+    #                                                            hour=15,
+    #                                                            minute=10,
+    #                                                            tzinfo=pytz.utc),
+    #                               length=timedelta(hours=1))
+    #     appointment.save()
+    #
+    #     factory = RequestFactory()
+    #     request = factory.post(reverse_lazy('connect_therapy:practitioner-make-notes',
+    #                                         kwargs={'pk': appointment.pk}))
+    #     request.user = self.request.user.practitioner
+    #     view = PractitionerNotesView()
+    #     view.request = request
+    #     view.get_object = lambda queryset=None: appointment
+    #     self.assertFalse(view.test_func())
+
+    # def test_test_func_when_email_not_confirmed(self):
+    #     self.practitioner.email_confirmed = False
+    #     self.practitioner.is_approved = True
+    #     self.practitioner.save()
+    #
+    #     factory = RequestFactory()
+    #     request = factory.post(reverse_lazy('connect_therapy:practitioner-make-notes',
+    #                                         kwargs={'pk': self.appointment.pk}))
+    #     request.user = self.practitioner.user
+    #     view = PractitionerNotesView()
+    #     view.request = request
+    #     view.get_object = lambda queryset=None: self.appointment
+    #     self.assertFalse(view.test_func())
+    #
+    # def test_test_func_when_email_confirmed(self):
+    #     self.practitioner.email_confirmed = True
+    #     self.practitioner.is_approved = True
+    #     self.patient.save()
+    #
+    #     factory = RequestFactory()
+    #     request = factory.post(reverse_lazy('connect_therapy:practitioner-make-notes',
+    #                                         kwargs={'pk': self.appointment.pk}))
+    #     request.user = self.practitioner.user
+    #     view = PractitionerNotesView()
+    #     view.request = request
+    #     view.get_object = lambda queryset=None: self.appointment
+    #     self.assertTrue(view.test_func())
+
+    # def test_practitioner_make_notes_before_appointment(self):
+    #     data = {
+    #         'practitioner_notes_before_meeting': 'Test make notes.'
+    #     }
+    #     form = PractitionerNotesForm(data=data)
+    #     form.is_valid()
+    #     factory = RequestFactory()
+    #     request = factory.post(reverse_lazy('connect_therapy:practitioner-make-notes',
+    #                                         kwargs={'pk': 1}))
+    #     view = PractitionerNotesView()
+    #     view.request = request
+    #     view.object = self.appointment
+    #     view.form_valid(form)
+    #     self.assertEqual(self.appointment.patient_notes_before_meeting,
+    #                      'Test make notes.')
+    #
+    # def test_post_when_form_valid(self):
+    #     data = {
+    #         'practitioner_notes_before_meeting': 'Test make notes.'
+    #     }
+    #     form = PractitionerNotesForm(data=data)
+    #     factory = RequestFactory()
+    #     request = factory.post(reverse_lazy('connect_therapy:practitioner-make-notes',
+    #                                         kwargs={'pk': 1}))
+    #     view = PractitionerNotesView()
+    #     view.request = request
+    #     view.get_object = lambda queryset=None: self.appointment
+    #     view.get_form = lambda form_class=None: form
+    #     view.post(request, 1)
+    #     self.assertEqual(self.appointment.patient_notes_before_meeting,
+    #                      'Test make notes.')
+    #
+    # def test_post_when_form_invalid(self):
+    #     data = {
+    #         'practitioner_notes_before_meeting': 'Test make notes.'
+    #     }
+    #     form = PractitionerNotesForm(data=data)
+    #     form.is_valid = lambda: False
+    #     factory = RequestFactory()
+    #     request = factory.post(reverse_lazy('connect_therapy:practitioner-make-notes',
+    #                                         kwargs={'pk': 1}))
+    #     view = PractitionerNotesView()
+    #     view.request = request
+    #     view.get_object = lambda queryset=None: self.appointment
+    #     view.get_form = lambda form_class=None: form
+    #     response = view.post(request, 1)
+    #     self.assertEqual(response.status_code, 200)
+    #
+    # def test_get_context_data(self):
+    #     factory = RequestFactory()
+    #     request = factory.post(reverse_lazy('connect_therapy:practitioner-make-notes',
+    #                                         kwargs={'pk': 1}))
+    #     view = PractitionerNotesView()
+    #     view.request = request
+    #     view.object = self.appointment
+    #     context = view.get_context_data()
+    #     self.assertEqual(len(context), 5)
