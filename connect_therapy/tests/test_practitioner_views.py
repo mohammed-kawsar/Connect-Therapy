bda40aee0a081fca7e61ac77d62f231db812c3b2
Robert Greener <robert.greener@kcl.ac.uk>
2018-03-26 19:38:19 +0100

    Add tests for PractitionerSetApppointmentView.test_func

--- connect_therapy/tests/test_practitioner_views.py
+++ connect_therapy/tests/test_practitioner_views.py
@@ -1119,6 +1119,67 @@ class TestChangePassword(TestCase):
         self.assertTrue(self.practitioner.user.check_password('woofwoof7'))
 
 
+class TestPractitionerSetAppointmentView(TestCase):
+    def setUp(self):
+        test_user_3 = User.objects.create_user(username='testauser3')
+        test_user_3.set_password('12345')
+
+        test_user_3.save()
+
+        self.practitioner = Practitioner(user=test_user_3,
+                                         address_line_1="My home",
+                                         postcode="EC12 1CV",
+                                         mobile="+447577293232",
+                                         bio="Hello",
+                                         email_confirmed=True,
+                                         is_approved=True)
+        self.practitioner.save()
+
+    def test_test_func_when_user_has_no_practitioner(self):
+        factory = RequestFactory()
+        request = factory.post(reverse_lazy('connect_therapy:practitioner-set-appointments'))
+        request.user = AnonymousUser()
+        view = PractitionerSetAppointmentView()
+        view.request = request
+        self.assertFalse(view.test_func())
+
+    def test_test_func_when_email_not_confirmed(self):
+        self.practitioner.email_confirmed = False
+        self.practitioner.is_approved = True
+        self.practitioner.save()
+
+        factory = RequestFactory()
+        request = factory.post(reverse_lazy('connect_therapy:practitioner-set-appointments'))
+        request.user = self.practitioner.user
+        view = PractitionerSetAppointmentView()
+        view.request = request
+        self.assertFalse(view.test_func())
+
+    def test_test_func_when_not_approved(self):
+        self.practitioner.email_confirmed = True
+        self.practitioner.is_approved = False
+        self.practitioner.save()
+
+        factory = RequestFactory()
+        request = factory.post(reverse_lazy('connect_therapy:practitioner-set-appointments'))
+        request.user = self.practitioner.user
+        view = PractitionerSetAppointmentView()
+        view.request = request
+        self.assertFalse(view.test_func())
+
+    def test_test_func_when_email_confirmed_and_is_approved(self):
+        self.practitioner.email_confirmed = True
+        self.practitioner.is_approved = True
+        self.practitioner.save()
+
+        factory = RequestFactory()
+        request = factory.post(reverse_lazy('connect_therapy:practitioner-set-appointments'))
+        request.user = self.practitioner.user
+        view = PractitionerSetAppointmentView()
+        view.request = request
+        self.assertTrue(view.test_func())
+
+
 class PractitionerLogoutTest(TestCase):
     def setUp(self):
         test_user_1 = User.objects.create_user(username='testuser1')
